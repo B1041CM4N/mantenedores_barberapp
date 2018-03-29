33a4ddd78f57186dab229497c203f067d6cc0e15
Ignacio Andrés Rodríguez Chandía <rodriguezch_ignacioandres@hotmail.cl>
2018-03-29 19:40:20 -0300

    CHG: modificaciones de orden y configuración de datos para conexion

--- DatabaseConfig.php
+++ DatabaseConfig.php
@@ -7,12 +7,4 @@
   $Password = "6defc4da778cdb20cee246d1807062e98f80d3304ac357a3";
   //Nombre de base de datos
   $DBName = "barberappdb";
-  // Define your host here.
-  // $HostName = "159.65.102.53";
-  // Define your database username here.
-  // $HostUser = "root";
-  // Define your database password here.
-  // $HostPass = "";
-  // Define your database name here.
-  // $DatabaseName = "barberappdb";
 ?>
